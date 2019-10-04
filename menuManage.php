9de3b6a2907e31fe07ebc278cfc18e0af525abaf
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-10-04 16:49:08 +0800

    Created interface for Menu management
    Thou need database to be setup for it to be completed.

---
+++ menuManage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" href="styles/style.css" />
+</head>
+
+<body>
+	<table>
+		<tr>
+			<th>Item no.</th>
+			<th>Item name</th>
+		</tr>
+		<tr>
+			<td>Placeholder 1</td>
+			<td>Placeholder 2</td>
+		</tr>
+	</table>
+	<div class="button_group">
+		<ul>
+			<li><button>Add Categories</button></li>
+			<li><button>Add Items</button></li>
+			<li><button>Edit Categories</button></li>
+			<li><button>Edit Items</button></li>
+		</ul>
+	</div>
+</body>
+
+</html>
