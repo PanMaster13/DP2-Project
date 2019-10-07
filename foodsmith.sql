3da8728c474926b4758fe4fb4105cedd3fe2470c
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-10-07 14:43:23 +0800

    Create foodsmith.sql
    Contains data to be used as database

---
+++ foodsmith.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 07, 2019 at 08:34 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `foodsmith`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `catID` varchar(4) NOT NULL,
+  `catName` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`catID`, `catName`) VALUES
+('c1', 'food'),
+('c2', 'drinks');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coupon`
+--
+
+CREATE TABLE `coupon` (
+  `couponCode` varchar(5) NOT NULL,
+  `couponName` text NOT NULL,
+  `couponAmount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `coupon`
+--
+
+INSERT INTO `coupon` (`couponCode`, `couponName`, `couponAmount`) VALUES
+('CP001', 'Friday Lazy Day Discount', 10),
+('CP002', 'Toto Ticket', 30),
+('CP003', 'Great Days Ticket', 15.5),
+('CP004', '1st Anniversary Ticket', 111.11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `itemID` varchar(4) NOT NULL,
+  `catID` varchar(4) NOT NULL,
+  `itemName` text NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`itemID`, `catID`, `itemName`, `price`) VALUES
+('1', 'c1', 'Burger', 8),
+('2', 'c1', 'Sandwich', 7),
+('3', 'c2', 'Milk tea', 3.5),
+('4', 'c2', 'Latte', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderlist`
+--
+
+CREATE TABLE `orderlist` (
+  `orderID` varchar(5) NOT NULL,
+  `date` date NOT NULL,
+  `itemList` varchar(100) NOT NULL,
+  `totalPrice` float NOT NULL,
+  `orderStatus` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `reportID` varchar(5) NOT NULL,
+  `date` date NOT NULL,
+  `orderAmount` int(11) NOT NULL,
+  `profit` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `report`
+--
+
+INSERT INTO `report` (`reportID`, `date`, `orderAmount`, `profit`) VALUES
+('R001', '2019-10-01', 15, 75.5),
+('R002', '2019-10-02', 50, 150.25),
+('R003', '2019-10-03', 40, 102.2),
+('R004', '2019-10-04', 20, 80.8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tables`
+--
+
+CREATE TABLE `tables` (
+  `tableID` varchar(5) NOT NULL,
+  `tableStatus` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tables`
+--
+
+INSERT INTO `tables` (`tableID`, `tableStatus`) VALUES
+('T001', 1),
+('T002', 1),
+('T003', 2),
+('T004', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userID` varchar(5) NOT NULL,
+  `userType` int(11) NOT NULL,
+  `userName` text NOT NULL,
+  `Password` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `userType`, `userName`, `Password`) VALUES
+('U001', 1, 'Frodo', '12345'),
+('U002', 2, 'Holo', 'ugaelmao'),
+('U003', 1, 'Hina', 'abcde');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`catID`);
+
+--
+-- Indexes for table `coupon`
+--
+ALTER TABLE `coupon`
+  ADD PRIMARY KEY (`couponCode`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`itemID`),
+  ADD KEY `catID` (`catID`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`reportID`);
+
+--
+-- Indexes for table `tables`
+--
+ALTER TABLE `tables`
+  ADD PRIMARY KEY (`tableID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `menu`
+--
+ALTER TABLE `menu`
+  ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`catID`) REFERENCES `category` (`catID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
