526299a628b83fce8ce88c0cd4f785eaa9248ec9
chrislow8683 <kururu1999@hotmail.com>
2019-10-07 11:51:18 +0800

    Added Admin Panel page Added basic UI and buttons Database wip

---
+++ adminPanel/adminPanel.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<title>Admin</title>
+	<meta charset="utf-8">
+	<meta name="author" content="Low Lip Shen">
+	<meta name="description" content="Admin Panel">
+	<meta name="keywords" content="">
+
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/335541e0f5.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<?php
+		include "../template/header.php";
+	?>
+	
+	<article>
+		<h1>Welcome Back Chris!</h1>
+		<div id="admin-panel">
+			<ul class="panel-selection-container">
+				<li class="panel-items color1">
+					<i class="fas fa-utensils fa-5x"></i>
+					<p>Menu Management</p>
+				</li>
+				<li class="panel-items color2">
+					<i class="fas fa-table fa-5x"></i>
+					<p>Table Management</p>
+				</li>
+				<li class="panel-items color3">
+					<i class="fas fa-receipt fa-5x"></i>
+					<p>Transaction Management</p>
+				</li>
+				<li class="panel-items color4">
+					<i class="fas fa-clipboard-list fa-5x"></i>
+					<p>Report Management</p>
+				</li>
+				<li class="panel-items color5">
+					<i class="fas fa-ticket-alt fa-5x"></i>
+					<p>Coupon Management</p>
+				</li>
+				<li class="panel-items color6">
+					<i class="fas fa-database fa-5x"></i>
+					<p>Export Database</p>
+				</li>
+			</ul>
+		</div>
+	</article>
+	
+	<footer>
+	
+	</footer>
+
+</body>
+
+
+
+
+</html>
---
+++ adminPanel/script.js
@@ -0,0 +1,24 @@
+//Codes taken from : https://jsfiddle.net/armaandhir/Lgt1j68s/
+function highlight_row() {
+    var table = document.getElementById('order-table');
+    var cells = table.getElementsByTagName('td');
+
+    for (var i = 0; i < cells.length; i++) {
+        // Take each cell
+        var cell = cells[i];
+        // do something on onclick event for cell
+        cell.onclick = function () {
+            // Get the row id where the cell exists
+            var rowId = this.parentNode.rowIndex;
+
+            var rowsNotSelected = table.getElementsByTagName('tr');
+            for (var row = 0; row < rowsNotSelected.length; row++) {
+                rowsNotSelected[row].classList.remove('selectedRow');
+            }
+            var rowSelected = table.getElementsByTagName('tr')[rowId];
+            rowSelected.className += " selectedRow";
+        }
+    }
+}
+
+window.onload = highlight_row;
