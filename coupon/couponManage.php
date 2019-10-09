70b9cf731c186ecb6b999b3664fb65753cb50653
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-10-09 12:40:53 +0800

    Implemented management interface for coupons
    Design similar to menu management page interface

---
+++ coupon/couponManage.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<title>Coupon Management Page</title>
+	<meta charset="utf-8">
+	<meta name="author" content="Jason">
+	
+	<link rel="stylesheet" href="couponStyle.css">
+	<script src="https://kit.fontawesome.com/335541e0f5.js" crossorigin="anonymous"></script>
+	
+<?php
+	include ("../template/header.php");
+?>
+
+	<h1>Coupon Management Page</h1>
+	<article>
+		
+		<div class="table-group">
+	<?php
+		$host = "127.0.0.1";
+		$username = "root";
+		$password = "";
+		$database = "foodsmith";
+		
+		// Create connection
+		$conn = new mysqli($host, $username, $password, $database);
+		
+		// Check connection
+		if (mysqli_connect_error())
+		{
+			die("Database connection failed: " . mysqli_connect_error());
+		}
+		
+		$couponQuery = "SELECT * FROM coupon";
+		$couponResult = $conn->query($couponQuery);
+		
+		echo "<table class='theTables'><tr><th>Coupon Code</th><th>Coupon Name</th><th>Coupon Discount Amount</th></tr>";
+		if ($couponResult->num_rows > 0)
+		{
+			while ($couponRow = $couponResult->fetch_assoc())
+			{
+				echo "<tr class='list-items'><td>" . $couponRow["couponCode"] . "</td><td>" . $couponRow["couponName"] . "</td><td>" . number_format($couponRow["couponAmount"], 2) . "</td></tr>";
+			}
+		}
+		else
+		{
+			echo "0 results";
+		}
+		echo "</table>";
+		mysqli_free_result($couponResult);
+		
+		// Close connection (although it is done automatically when script ends
+		$conn->close();
+	?>
+		</div>
+		
+		<div class='coupon-buttons-group'>
+			<button class='coupon-buttons'>
+				<p>Add Coupon</p>
+			</button>
+			<button class='coupon-buttons'>
+				<p>Edit Coupon</p>
+			</button>
+			<button class='coupon-buttons'>
+				<p>Delete Coupon</p>
+			</button>
+		</div>
+		
+	</article>
+	
+	<footer>
+	</footer>
+	<script src="couponScript.js"></script>
+</body>
+</html>
---
+++ coupon/couponScript.js
@@ -0,0 +1,31 @@
+//Codes taken from : https://jsfiddle.net/armaandhir/Lgt1j68s/
+function highlight_row() {
+    var tableList = document.getElementsByClassName("theTables");
+	
+	for (var x = 0; x < tableList.length; x++)
+	{
+		var cells = tableList[x].getElementsByTagName("td");
+		
+		for (var i = 0; i < cells.length; i++) {
+			
+			var cell = cells[i];
+			
+			cell.onclick = function(){
+				//Deselects all other rows
+				for (var y = 0; y < tableList.length; y++)
+				{
+					var otherRows = tableList[y].getElementsByTagName("tr");
+					for (var j = 0; j < otherRows.length; j++)
+					{
+						otherRows[j].classList.remove("selectedRow");
+					}
+				}
+			
+				// Sets clicked row with the class name
+				this.parentNode.className = "selectedRow";
+			}
+		}
+	}
+}
+
+window.onload = highlight_row;
