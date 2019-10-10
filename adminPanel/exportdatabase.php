afeb0d2ff9fa6ca17dfa8f4c0f252b5aac1f6bb5
chrislow8683 <kururu1999@hotmail.com>
2019-10-10 10:49:25 +0800

    Added database exportation functionality and adminPanel onclick buttons -minor file name changes -most buttons workable in adminPanel -database exportation works

---
+++ adminPanel/exportdatabase.php
@@ -0,0 +1,22 @@
+<!--Referenced from-->
+<!--https://stackoverflow.com/questions/2170182/how-to-backup-mysql-database-in-php-->
+
+<?php
+   $dbhost = '127.0.0.1';
+   $dbuser = 'root';
+   $dbpass = '';
+   $dbname = 'foodsmith';
+   
+   $backup_file = "backupfile\\" . $dbname . date("Y-m-d-H-i-s") . '.sql';
+   $command = "mysqldump --u $dbuser -p $dbname > $backup_file";
+   
+   exec($command, $output);
+   
+   if($output=' '){
+		echo "Exported.";
+   }
+   else{
+	   echo "Error.";
+	   var_dump($output);
+   }
+?>
